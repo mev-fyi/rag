22c00fa1b417ca9eff05f80b91a685cb635e6c5c
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-10-06 13:09:13 +0200

    features: add setup bash script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# create virtual environment
+python -m venv venv
+
+# activate virtual environment
+source venv/bin/activate
+
+# install packages from requirements.txt
+pip install -r requirements.txt
