d9a0bd4bd572ab8950e6b944fd4d799724b2b95c
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-10-13 17:11:26 +0200

    features: add rule-based cleaning of common AssemblyAI .mp3 -> text mistakes across all YouTube diarized content

--- rag/Llama_index_sandbox/data_ingestion_youtube/load/clean_transcripts_utterances.py
+++ rag/Llama_index_sandbox/data_ingestion_youtube/load/clean_transcripts_utterances.py
@@ -10,3 +10,48 @@ items to clean:
 - web three -> web3
 - SWAV -> SUAVE
 """
+
+import os
+
+from rag.Llama_index_sandbox import root_directory
+
+
+def correct_typos_in_files():
+
+    """
+    Correct specific typos in .txt files under a given directory.
+
+    Args:
+    - root_dir (str): Path to the root directory where search begins.
+    """
+    root_dir = f"{root_directory()}/datasets/evaluation_data/diarized_youtube_content_2023-10-06"
+
+    # Dictionary of typos and their corrections
+    typo_dict = {
+        "L Two": "L2",
+        "L Two s": "L2s",
+        "L Three": "L3s",
+        "Arbitram": "Arbitrum",
+        "web three": "web3",
+        "SWAV": "SUAVE"
+    }
+
+    # Walk through root_dir
+    for dirpath, _, filenames in os.walk(root_dir):
+        for fname in filenames:
+            if fname.endswith("_diarized_content_processed_diarized.txt"):
+                file_path = os.path.join(dirpath, fname)
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+                    for typo, correction in typo_dict.items():
+                        content = content.replace(typo, correction)
+                        print("Corrected typo: ", typo, " -> ", correction, " in ", file_path, "\n")
+
+                # Write corrected content back to file
+                with open(file_path, 'w', encoding='utf-8') as file:
+                    file.write(content)
+
+
+if __name__ == "__main__":
+    correct_typos_in_files()
+
