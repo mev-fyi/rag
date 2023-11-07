36c932c7ed011a20f5a9a4623f09a5f57cf267a8
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-07 22:50:35 +0100

    features: update flask SSE

--- src/Llama_index_sandbox/app.py
+++ src/Llama_index_sandbox/app.py
@@ -1,89 +1,90 @@
+import json
 import logging
-import threading
-import uuid
 import os
-from flask import Flask, request, jsonify
+import threading
+import time
+import uuid
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 from google.cloud import firestore
-
-from src.Llama_index_sandbox.gcs_utils import set_secrets_from_cloud, get_firestore_client
 from src.Llama_index_sandbox.main import initialise_chatbot
 from src.Llama_index_sandbox.retrieve import ask_questions
-
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "https://www.mev.fyi"}})
 
+# Initialize the chatbot
 engine = 'chat'
 query_engine_as_tool = True
 recreate_index = False
 retrieval_engine, query_engine, store_response_partial = initialise_chatbot(engine=engine, query_engine_as_tool=query_engine_as_tool, recreate_index=recreate_index)
-set_secrets_from_cloud()
-db = get_firestore_client()  # Initialize Firestore DB
+
+# Initialize Firestore DB
+db = firestore.Client()
 
 
 @app.route('/healthz')
 def health():
-    # Optionally add any vital health checks your app needs.
     return 'OK', 200
 
 
 def background_processing(message, job_id):
     try:
-        response, formatted_metadata = ask_questions(input_queries=[message], retrieval_engine=retrieval_engine,
-                                                     query_engine=query_engine, store_response_partial=store_response_partial,
-                                                     engine=engine, query_engine_as_tool=query_engine_as_tool, run_application=True)
-
-        response = f"{response} \n\n{formatted_metadata}"
-        logging.info(f"Completed processing for job {job_id}. Storing response in Firestore.")
+        response, formatted_metadata = ask_questions(
+            input_queries=[message],
+            retrieval_engine=retrieval_engine,
+            query_engine=query_engine,
+            store_response_partial=store_response_partial,
+            engine=engine,
+            query_engine_as_tool=query_engine_as_tool,
+            run_application=True
+        )
 
         # Save the response to Firestore
-        doc_ref = db.collection('chat_responses').document(job_id)
-        doc_ref.set({
-            'response': response,
+        db.collection('chat_responses').document(job_id).set({
+            'response': f"{response} \n\n{formatted_metadata}",
             'timestamp': firestore.SERVER_TIMESTAMP
         })
 
     except Exception as e:
         logging.error(f"Error processing job {job_id}: {e}")
+        db.collection('chat_responses').document(job_id).set({
+            'response': f"Error: {e}",
+            'timestamp': firestore.SERVER_TIMESTAMP
+        })
 
 
 @app.route('/chat', methods=['POST'])
 def chat_endpoint():
-    # Get message from the POST request
     data = request.get_json()
     message = data.get("message")
 
     if not message:
         return jsonify({"error": "Message not provided"}), 400
 
-    logging.info(f"Received message: {message}")
-
-    # Generate a unique job ID
     job_id = str(uuid.uuid4())
-
-    # Spawn a new thread for the long-running task
     thread = threading.Thread(target=background_processing, args=(message, job_id))
     thread.start()
 
-    # Immediately acknowledge the request
     return jsonify({"status": "processing", "job_id": job_id}), 202
 
 
-@app.route('/result/<job_id>', methods=['GET'])
-def get_result(job_id):
-    try:
+@app.route('/stream/<job_id>')
+def stream(job_id):
+    def generate():
+        # Create an event stream from Firestore
         doc_ref = db.collection('chat_responses').document(job_id)
-        doc = doc_ref.get()
-        if doc.exists:
-            return jsonify({"response": doc.to_dict()}), 200
-        else:
-            return jsonify({"error": "Result not found or still processing"}), 404
-    except Exception as e:
-        logging.error(f"Error retrieving job {job_id}: {e}")
-        return jsonify({"error": "Internal Server Error"}), 500
+
+        # Check for response in Firestore and yield when available
+        while True:
+            doc = doc_ref.get()
+            if doc.exists:
+                yield f"data: {json.dumps(doc.to_dict())}\n\n"
+                break
+            time.sleep(1)
+    return Response(generate(), content_type='text/event-stream')
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=True)
