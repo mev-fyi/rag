adaa7f644abe27f1de33eda1bc67fdb967bd8934
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-11-27 16:48:35 +0100

    feat: add method to generate research paper thumbnails

--- src/Llama_index_sandbox/utils.py
+++ src/Llama_index_sandbox/utils.py
@@ -1,6 +1,7 @@
 import time
 import logging
 import os
+import fitz
 from datetime import datetime
 from functools import wraps
 
@@ -521,9 +522,42 @@ def process_messages(data):
     return chat_messages
 
 
+def generate_pdf_thumbnails(pdf_directory, output_directory, thumb_scale=2, page_fraction=0.6):
+    """
+    Generate high-resolution thumbnails for a portion of the first page of all PDFs.
+
+    Args:
+    pdf_directory (str): Directory containing PDF files.
+    output_directory (str): Directory to save the thumbnails.
+    thumb_scale (float): Scale factor for the thumbnail resolution.
+    page_fraction (float): Fraction of the page to be shown in the thumbnail (0 to 1).
+    """
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    for filename in os.listdir(pdf_directory):
+        if filename.lower().endswith('.pdf'):
+            pdf_path = os.path.join(pdf_directory, filename)
+            thumbnail_path = os.path.join(output_directory, os.path.splitext(filename)[0] + '.png')
+
+            # Open the PDF
+            with fitz.open(pdf_path) as doc:
+                page = doc.load_page(0)  # First page
+                # Define the rectangle for the specified portion of the page
+                portion_rect = fitz.Rect(0, 0, page.rect.width, page.rect.height * page_fraction)
+
+                # Create a pixmap for the specified rectangle with increased resolution
+                matrix = fitz.Matrix(thumb_scale, thumb_scale).prescale(portion_rect.width / page.rect.width, portion_rect.width / page.rect.width)
+                pix = page.get_pixmap(matrix=matrix, clip=portion_rect)
+                pix.save(thumbnail_path)
+                print(f"High-resolution thumbnail generated for {filename} at {thumbnail_path}")
+
+
 if __name__ == '__main__':
     directory = f"{root_directory()}/datasets/evaluation_data/diarized_youtube_content_2023-10-06"
+    pdf_dir = f"{root_directory()}/datasets/evaluation_data/baseline_evaluation_research_papers_2023-10-05"
     # print_frontend_content()
-    delete_mp3_if_text_or_json_exists(directory)
+    # delete_mp3_if_text_or_json_exists(directory)
     # save_data_into_zip()
     # copy_txt_files_to_transcripts()
+    generate_pdf_thumbnails(pdf_directory=pdf_dir, output_directory=f'{root_directory()}/datasets/pdf_thumbnails')
