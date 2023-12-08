e9246792feb7876937712516d3404259795ce271
vmeylan <valentin.meylan@alumni.epfl.ch>
2023-12-08 19:48:12 +0100

    feat: update node reranker to work with default keys

--- src/Llama_index_sandbox/custom_react_agent/tools/reranker/custom_query_engine.py
+++ src/Llama_index_sandbox/custom_react_agent/tools/reranker/custom_query_engine.py
@@ -15,14 +15,14 @@ class CustomQueryEngine(RetrieverQueryEngine):
             'writings.flashbots': 1,
             'frontier.tech': 0.95,
             'research.anoma': 0.95,
-            'remainder': 0.9
+            'default': 0.9
         },
         'youtube_weights': {
             'Bell Curve 2023': 0.95,
             'Fenbushi Capital': 0.9,
             'SevenX Ventures': 0.9,
             'Tim Roughgarden Lectures': 0.9,
-            'remainder': 0.8,
+            'default': 0.8,
         },
     }
     authors_list = {
@@ -62,17 +62,29 @@ class CustomQueryEngine(RetrieverQueryEngine):
             # Document type weight adjustments
             weight_key = document_type.lower() + '_weights'
             if weight_key in self.document_weights:
+                matched = False
                 for source, weight in self.document_weights[weight_key].items():
-                    if source in link or source == 'remainder':
+                    if source in link:
                         score *= weight
+                        matched = True
                         break  # Break the loop once a match is found
+                if not matched and 'default' in self.document_weights[weight_key]:
+                    score *= self.document_weights[weight_key]['default']
 
             # Author weight adjustment
+            author_matched = False
             for author in authors.split(','):
                 author = author.strip()
                 for firm, authors_in_firm in self.authors_list.items():
                     if author in authors_in_firm:
                         score *= self.authors_weights.get(firm, 1)
+                        author_matched = True
+                        break  # Break the inner loop if an author matches
+                if author_matched:
+                    break  # Break the outer loop if any author matches
+
+            if not author_matched and 'default' in self.authors_weights:
+                score *= self.authors_weights['default']  # Apply default weight for authors if no author matched
 
             node_with_score.score = score
         return nodes_with_score
