cafbd90c91a7f2485c153dad6af349b27756b78e
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-01-31 22:18:05 +0100

    fix: cloudbuild, docker

--- startup.sh
+++ startup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# Install Docker (if not already installed)
-sudo apt-get update
-sudo apt-get install -y docker.io
+# Install Docker if not already installed
+if ! command -v docker &> /dev/null
+then
+    sudo apt-get update
+    sudo apt-get install -y docker.io
+fi
 
-# Pull the Docker image
-docker pull gcr.io/mev-fyi/twitter-bot-app:$SHORT_SHA
+# Authenticate Docker with GCR (Assumes gcloud is installed and configured)
+gcloud auth configure-docker
+
+# Pull the latest Docker image
+docker pull gcr.io/mev-fyi/twitter-bot-app:latest
 
 # Run the Docker container
-docker run -d --name my_twitter_bot_container gcr.io/mev-fyi/twitter-bot-app:$SHORT_SHA
+docker run -d --name my_twitter_bot_container gcr.io/mev-fyi/twitter-bot-app:latest
