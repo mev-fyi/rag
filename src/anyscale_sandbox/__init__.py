70173672b12f6e6d049da0ed5e0f715f57486831
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-01 14:48:37 +0100

    FIX: delete anyscale directory, update all paths which previously used root_directory() method from anyscale

--- src/anyscale_sandbox/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.Llama_index_sandbox import root_directory
-
-root_dir = root_directory()
-mev_fyi_dir = f"{root_dir}/../mev.fyi/"
-research_papers_dir = f"{mev_fyi_dir}/data/paper_details.csv"
-pdfs_dir = f"{mev_fyi_dir}/data/papers_pdf_downloads/"
