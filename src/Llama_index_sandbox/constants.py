58f4c4bb765426b8518d56da2787293bb57b35b2
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-02-09 16:29:49 +0100

    FIX: add `gpt-3.5-turbo-0125` to OPENAI_INFERENCE_MODELS list

--- src/Llama_index_sandbox/constants.py
+++ src/Llama_index_sandbox/constants.py
@@ -37,9 +37,9 @@ TEXT_SPLITTER_CHUNK_OVERLAP_PERCENTAGE = 10
 '''
 valid OpenAI model name in: gpt-4, gpt-4-32k, gpt-4-0613, gpt-4-32k-0613, gpt-4-0314, gpt-4-32k-0314, gpt-3.5-turbo, gpt-3.5-turbo-16k, gpt-3.5-turbo-0613, 
 gpt-3.5-turbo-16k-0613, gpt-3.5-turbo-0301, text-davinci-003, text-davinci-002, gpt-3.5-turbo-instruct, 
-text-ada-001, text-babbage-001, text-curie-001, ada, babbage, curie, davinci, gpt-35-turbo-16k, gpt-35-turbo
+text-ada-001, text-babbage-001, text-curie-001, ada, babbage, curie, davinci, gpt-35-turbo-16k, gpt-35-turbo, gpt-3.5-turbo-0125
 '''
-OPENAI_INFERENCE_MODELS = ["gpt-4", "gpt-4-32k", "gpt-4-0613", "gpt-4-32k-0613", "gpt-4-0314", "gpt-4-32k-0314", "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0613",
+OPENAI_INFERENCE_MODELS = ["gpt-3.5-turbo-0125", "gpt-4", "gpt-4-32k", "gpt-4-0613", "gpt-4-32k-0613", "gpt-4-0314", "gpt-4-32k-0314", "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0613",
 "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-0301", "text-davinci-003", "text-davinci-002", "gpt-3.5-turbo-instruct", "gpt-35-turbo-16k", "gpt-35-turbo", "gpt-4-1106-preview", "gpt-4-turbo", "gpt-4-turbo-1106"]
 
 
