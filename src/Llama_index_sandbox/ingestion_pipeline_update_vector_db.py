862fd858a61835b630e13b105764771c93135517
vmeylan <valentin.meylan@alumni.epfl.ch>
2024-03-12 11:59:40 +0100

    feat: update pipeline upgrade script

--- src/Llama_index_sandbox/ingestion_pipeline_update_vector_db.py
+++ src/Llama_index_sandbox/ingestion_pipeline_update_vector_db.py
@@ -18,7 +18,7 @@ from llama_index.core.ingestion import (
     IngestionPipeline,
 )
 from llama_index.core.node_parser import SentenceSplitter
-from src.Llama_index_sandbox.utils.utils import timeit, get_embedding_model, root_directory
+from src.Llama_index_sandbox.utils.utils import timeit, get_embedding_model, root_directory, copy_and_verify_files
 
 
 def load_vector_store_from_pinecone_database(delete_old_index=False):
@@ -29,6 +29,15 @@ def load_vector_store_from_pinecone_database(delete_old_index=False):
     if delete_old_index:
         # pass
         pc.delete_index(index_name)
+        # Dimensions are for text-embedding-ada-002
+        from pinecone import ServerlessSpec
+        pc.create_index(
+            "mevfyi",
+            dimension=1536,
+            metric="euclidean",
+            spec=ServerlessSpec(cloud="aws", region="us-west-2"),
+        )
+
     pinecone_index = pc.Index(index_name)
     vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
     return vector_store
@@ -48,8 +57,8 @@ def initialise_pipeline(add_to_vector_store=True, delete_old_index=False):
             embedding_model,
         ],
         vector_store=vector_store,
-        # docstore_strategy=DocstoreStrategy.DUPLICATES_ONLY,
-        docstore_strategy=DocstoreStrategy.UPSERTS,
+        docstore_strategy=DocstoreStrategy.DUPLICATES_ONLY,
+        # docstore_strategy=DocstoreStrategy.UPSERTS,
         docstore=SimpleDocumentStore(),
     )
     if os.path.exists(f"{root_directory()}/pipeline_storage"):
@@ -62,7 +71,8 @@ def initialise_pipeline(add_to_vector_store=True, delete_old_index=False):
 
 
 @timeit
-def create_index(add_new_transcripts=False, num_files=2):
+def create_index(add_new_transcripts=False, num_files=None):
+    copy_and_verify_files()
     logging.info("Starting Index Creation Process")
     # TODO 2024-03-06: need to locally store the embeddings. That requires going offline since with the pipeline we first delete the index
     #  then do the embedding and in the same pipeline method, insert the vectors, and only then we persist to the docstore.
@@ -81,23 +91,28 @@ def create_index(add_new_transcripts=False, num_files=2):
     #   3. save to docstore
     #   4. instantiate a new pipeline, delete the vector store, add the vector store, and add the nodes of the first pipeline to the vector store of the second pipeline
 
-    overwrite = False
+    overwrite = True  # whether we overwrite DB namely we load all documents instead of only loading the increment since last database update
+    num_files = 1
+    files_window = None# (20, 100)
+
+    # TODO 2024-03-12: the whole process can be largely improved if we batched process end to end the docs to inserting the vectors
 
     # STEP 1: load all docs
-    documents_pdfs = load_pdf.load_pdfs(directory_path=Path(PDF_DIRECTORY), num_files=num_files, overwrite=overwrite)
-    documents_pdfs += load_articles.load_pdfs(directory_path=Path(ARTICLES_DIRECTORY), num_files=num_files, overwrite=overwrite)
-    documents_pdfs += load_discourse_articles.load_pdfs(directory_path=Path(DISCOURSE_ARTICLES_DIRECTORY), num_files=num_files, overwrite=overwrite)
-    documents_pdfs += load_docs.load_docs_as_pdf(num_files=num_files, overwrite=overwrite)
-    documents_youtube = load_video_transcripts(directory_path=Path(YOUTUBE_VIDEO_DIRECTORY), add_new_transcripts=add_new_transcripts, num_files=num_files, overwrite=overwrite)
+    documents_pdfs = load_pdf.load_pdfs(directory_path=Path(PDF_DIRECTORY), num_files=0, files_window=files_window, overwrite=overwrite)
+    documents_pdfs += load_docs.load_docs_as_pdf(num_files=0, files_window=files_window, overwrite=overwrite)
+    documents_pdfs += load_articles.load_pdfs(directory_path=Path(ARTICLES_DIRECTORY), num_files=0, files_window=files_window, overwrite=overwrite)
+    documents_pdfs += load_discourse_articles.load_pdfs(directory_path=Path(DISCOURSE_ARTICLES_DIRECTORY), num_files=num_files, files_window=files_window, overwrite=overwrite)
+    documents_youtube = load_video_transcripts(directory_path=Path(YOUTUBE_VIDEO_DIRECTORY), add_new_transcripts=add_new_transcripts, num_files=None, files_window=files_window, overwrite=overwrite)
 
     # STEP 2: EMBED ALL DOCS + SAVE TO DOCSTORE LOCALLY WITHOUT VECTOR STORE (TO BE USED IN STEP 4)
-    pipeline_v1 = initialise_pipeline(add_to_vector_store=False)
-    nodes = pipeline_v1.run(documents=documents_pdfs + documents_youtube, num_workers=int(os.cpu_count())-3, show_progress=True)
-    pipeline_v1.persist(persist_dir=f"{root_directory()}/pipeline_storage")
+    pipeline_v1 = initialise_pipeline(add_to_vector_store=True)
+    nodes = pipeline_v1.run(documents=documents_pdfs + documents_youtube, num_workers=15, show_progress=True)
+    if len(nodes) > 0:
+        pipeline_v1.persist(persist_dir=f"{root_directory()}/pipeline_storage")
     logging.info(f"Processed {len(nodes)} Nodes")
 
     # STEP 3: SPIN OFF NEW PIPELINE INSTANCE WITH VECTOR STORE WHICH IS DELETED AND CREATED BACK
-    pipeline_v2 = initialise_pipeline(delete_old_index=True)
+    pipeline_v2 = initialise_pipeline()
     # STEP 4: ADD EMBEDDINGS TO VECTOR STORE
     if pipeline_v2.vector_store is not None:
         nodes_to_add = [n for n in nodes if n.embedding is not None]
